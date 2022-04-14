4bebe7dd31c19b92964d13774efdf6a9f155e7a4
mohamed1408 <mohamedanastsi@gmail.com>
2022-04-14 16:08:41 +0530

    login_page_complete

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -4,9 +4,12 @@ import {
   View,
   Image,
   Dimensions,
+  TextInput,
+  TouchableOpacity,
 } from "react-native";
+import { BlurView } from "expo-blur";
+import { LinearGradient } from "expo-linear-gradient";
 
-import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View as TView } from "../components/Themed";
 import { RootStackScreenProps } from "../types";
 import images from "../assets/images";
@@ -25,7 +28,38 @@ export default function LoginScreen({
       >
         <Image source={images.logo} style={styles.toplogo} />
         <View style={styles.bottomview}>
-          <Text>asdasdasd</Text>
+          <Text style={styles.title}>login</Text>
+          <Text style={styles.subtitle}>
+            Enter valid mobile number, we will send you an OTP for verifaction.
+          </Text>
+
+          <BlurView intensity={50} style={styles.card}>
+            <View style={styles.cardbody}>
+              <Text style={[styles.cardtitle]}>mobile number</Text>
+
+              <View style={styles.inputcontainer}>
+                <TextInput style={[styles.input, { flex: 2 }]} value="+91" />
+                <TextInput
+                  style={[styles.input, { flex: 8 }]}
+                  value="7032112233"
+                />
+              </View>
+            </View>
+
+            <View style={styles.cardfooter}>
+              <TouchableOpacity style={styles.btn}>
+                <LinearGradient
+                  style={styles.linearbg}
+                  key={"asdsad"}
+                  colors={["#f44e4e", "#fdbe20"]}
+                  start={[0, 1]}
+                  end={[1, 0]}
+                >
+                  <Text style={styles.btntext}>get OTP</Text>
+                </LinearGradient>
+              </TouchableOpacity>
+            </View>
+          </BlurView>
         </View>
       </ImageBackground>
     </View>
@@ -40,14 +74,6 @@ const styles = StyleSheet.create({
   image: {
     flex: 1,
     justifyContent: "center",
-  },
-  text: {
-    color: "white",
-    fontSize: 42,
-    lineHeight: 84,
-    fontWeight: "bold",
-    textAlign: "center",
-    backgroundColor: "#000000c0",
   },
   toplogo: {
     width: 45,
@@ -65,6 +91,79 @@ const styles = StyleSheet.create({
     bottom: 0,
     borderColor: "white",
     width: "100%",
+    borderWidth: 0,
+    padding: 20,
+    paddingBottom: 0,
+  },
+  title: {
+    color: "white",
+    fontSize: 30,
+    fontWeight: "bold",
+    fontFamily: "questrial-regular",
+    paddingHorizontal: 15,
+  },
+  subtitle: {
+    color: "white",
+    marginTop: 10,
+    fontFamily: "questrial-regular",
+    lineHeight: 20,
+    fontSize: 17,
+    paddingHorizontal: 15,
+  },
+  card: {
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    padding: 15,
+    marginTop: 50,
+    paddingHorizontal: 15,
+  },
+  cardbody: {
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 20,
+  },
+  cardtitle: {
+    color: "#457c78",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  inputcontainer: {
+    backgroundColor: "#eaf4f3",
+    borderColor: "#cde8e6",
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 10,
+    marginVertical: 20,
+    flex: 1,
+    flexDirection: "row",
+  },
+  input: {
+    fontWeight: "bold",
+    fontSize: 20,
+    color: "#00a89b",
+    letterSpacing: 3,
+  },
+  cardfooter: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 25,
+  },
+  btn: {
+    borderColor: "white",
     borderWidth: 1,
+    borderRadius: 50,
+    width: "80%",
+  },
+  linearbg: {
+    width: "100%",
+    padding: 15,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 50,
+  },
+  btntext: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 20,
   },
 });
